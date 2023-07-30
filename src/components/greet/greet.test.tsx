be8f2b7478ed9bc64cb3d2aadaefd8fcd7220331
Koushik-Deb <koushik.imon@gmail.com>
2023-07-30 12:21:44 +0600

    JEST

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders correctly ", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/Hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Renders correctly ", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
-
-test("Greet renders with a name", () => {
-  render(<Greet name="Koushik" />);
-  const textElement = screen.getByText("Hello Koushik");
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("Renders with a name", () => {
+    render(<Greet name="Koushik" />);
+    const textElement = screen.getByText("Hello Koushik");
+    expect(textElement).toBeInTheDocument();
+  });
 });
